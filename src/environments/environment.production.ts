3198472ffd98722a5d4fe74d3cdf86a78bf04e1f
Andres Galeano <anfegave98@hotmail.com>
2026-06-07 11:04:45 -0500

    chore(environment): Se agrega url api

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://REPLACE_WITH_BACKEND_HOST/api/v1',
+  apiUrl: 'https://worldcuppool-api.onrender.com',
   encryption: {
     enabled: true,
     key: 'gOxokGUU1PD+0Zp52tMjHif70MF8nR1ameX0inQg7Ck=',
