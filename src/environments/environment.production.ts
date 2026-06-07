e95ca9fa5f7ab5aba08c484192f9d95ecdb483dd
Andres Galeano <anfegave98@hotmail.com>
2026-06-07 12:57:57 -0500

    chore(environment): Se ajusta las variables de entorno

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://worldcuppool-api.onrender.com',
+  apiUrl: '',
   encryption: {
     enabled: true,
-    key: 'gOxokGUU1PD+0Zp52tMjHif70MF8nR1ameX0inQg7Ck=',
-    iv:  'p76iDDQAhbdUXmgRcthukQ==',   
+    key: '',
+    iv:  '',   
   },
 };
